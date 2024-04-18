5040ee37fae3ef8328bafd9c66edb8f6d5804bcc
JasonMckee-Bakos <jasonmckee44@yahoo.com>
2024-04-18 14:19:14 -0500

    Added a function to count each character

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     bookPath = "books/frankenstein.txt"
     text = getText(bookPath)
     num_words = countWords(text)
-    print(num_words)
+    charCount = count_characters(text)
+
+    print(charCount)
 
 def countWords(text):
     wordCount = text.split()
@@ -12,6 +14,14 @@ def getText(path):
        with open(path) as f:
         file_contents = f.read()
         return file_contents
-    
+       
+def count_characters(text):
+    charDict = {}
+    for char in text.lower():
+        if char not in charDict:
+            charDict[char] = 1
+        else:
+            charDict[char] += 1
+    return charDict
 
 main()
